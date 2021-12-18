50d7b76c9becbaca27ac60f6762fd2f64cc4f220
Qiaozenghan <616555477@qq.com>
2021-12-18 19:32:17 +0800

    1

--- src/main/java/com/example/demo/test/Test.java
+++ src/main/java/com/example/demo/test/Test.java
@@ -5,15 +5,5 @@ public class Test {
         System.out.println(1111);
     }
 
-    /**
-     *  lalalalalal
-     * @return void
-     * @author Qiaozenghan  is
-     * @date 2021/12/6 下午10:24
-    */
-    public void getTest12(){
-        String str12 = "s";
-        System.out.println(str12);
-    }
-    //a b c
+   //1
 }
