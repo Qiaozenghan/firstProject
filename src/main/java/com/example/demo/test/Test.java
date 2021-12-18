f540e96dd5454c0f9314b58e6bfcade8e747dc51
Qiaozenghan <616555477@qq.com>
2021-12-18 10:56:48 +0800

    first commit

---
+++ src/main/java/com/example/demo/test/Test.java
@@ -0,0 +1,18 @@
+package com.example.demo.test;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(1111);
+    }
+    
+    /**
+     * 
+     * @return void
+     * @author Qiaozenghan
+     * @date 2021/12/6 下午10:24
+    */
+    public void getTest(){
+        String str = "s";
+        System.out.println(str);
+    }
+}
